21edee76ee25feb93466de139fb4e9935c963478
calvin PB <noname729@gmail.com>
2020-11-03 01:12:56 +0800

    Added Event to submit button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
-var name = document.getElementById("eeName").value;
-var salary = document.getElementById("hireDate").value;
-var hireDate = document.getElementById("salary").value;
+var hireDate = document.getElementById("hireDate").value;
+var form = document.getElementById('hrData');
 
-// alert(name);
-// console.log(name);
-// console.log(salary);
-// console.log(hireDate);
+
+function display(){
+    // console.log(name);
+};
+
+form.addEventListener('submit',function(event){
+    var name = document.getElementById("eeName").value;
+    var salary = document.getElementById("salgitary2").value;
+
+
+    if (!name || !salary){
+        alert("Please enter value on the fields");
+    }
+    else{
+    console.log(name);
+    event.preventDefault();
+}});
