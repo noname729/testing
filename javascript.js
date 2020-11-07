ff75a47060489df1bd927b791346dfabfb6c6c9c
Calvin <noname729@gmail.com>
2020-11-07 18:18:14 +0800

    Add reset button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
+//givar eeName = document.getElementById("eeName").value;
 var hireDate = document.getElementById("hireDate").value;
 var form = document.getElementById('hrData');
 
 
-function display(){
-    console.log("YO");
+function display(name) {
+
+    console.log("Hi, " + name);
     event.preventDefault();
 };
 
