2d9eabd32c4c045c889efdb058c119e24bee03d3
calvin PB <noname729@gmail.com>
2020-11-12 01:41:37 +0800

    add AL

--- javascript.js
+++ javascript.js
@@ -2,10 +2,13 @@ var hireDate = document.getElementById("hireDate").value;
 var form = document.getElementById('hrData');
 
 
-function display(){
-    console.log("YO");
+function display() {
+    var eeName = document.getElementById('eeName').value;
+    var salary = document.getElementById('salary').value;
+    var annualLeave = document.getElementById('annualLeave').value;
+
+    
+    console.log("Hi, " + eeName + ". Your salary is " + salary );
     event.preventDefault();
 };
-
-
 
